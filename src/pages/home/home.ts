af4cf858baa267c2dfdc241bd18eb39df8035398
fermin <fmolinuevo@velocitypartners.net>
2017-09-04 10:00:26 -0300

    Added sidebar

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NewTravel } from '../new-travel/new-travel';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  addTravel(){
+     this.navCtrl.push(NewTravel);
   }
 
 }
---
+++ src/pages/new-travel/new-travel.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'new-travel',
+  templateUrl: 'new-travel.html'
+})
+export class NewTravel {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
